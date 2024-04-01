be553d37daaafe539540ae99fb18245c9dba05db
Melih Berkay Aydin <berkaydin34@gmail.com>
2024-04-01 23:13:08 +0200

    initial header and quiz part is created

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [userAnswers, setuserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setuserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
